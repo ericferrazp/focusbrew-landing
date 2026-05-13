51b37fcba4181a8457015327489e33545b20c620
Eric Ferraz <ericferrazp@gmail.com>
2026-05-13 18:05:51 -0300

    feat: add features section with three cards

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
-      <section className="flex flex-col mx-auto px-4 py-2 items-center mt-60 border border-red-500 max-w-3xl">
+      <section className="flex flex-col mx-auto px-4 py-2 items-center mt-60 max-w-3xl">
         <h1 className ="mb-2 text-red-200 font-bold text-4xl">
           FocusBrew! Your assistant app to Podomoro!
         </h1>
@@ -16,22 +16,24 @@ export default function Home() {
       </section>
 
       <section>
-      <h2 className="text-center mt-10 font-bold text-3xl border border-red-500 mx-auto max-w-2xl">
+      <h2 className="text-center mt-10 font-bold text-3xl mx-auto max-w-2xl mb-7">
         Why FocusBrew?
       </h2>
-      <div className= 'grid grid-cols-3 gap-6 border border-red-500 mx-auto max-w-5xl mt-3'>
+      <div className= 'grid grid-cols-3 gap-6 mx-auto max-w-5xl mt-3 p-4'>
 
-        <div className='flex flex-col mx-auto'>
+        <div className='flex flex-col mx-auto bg-zinc-900 rounded-xl border border-zinc-800 p-3'>
           <h3 className ='text-center'> Pomodoro Timer </h3>
-          <p>Customizable focus sessions for deep work</p>
+          <p className='text-center'>Customizable focus sessions for deep work</p>
         </div>
 
-        <div className='flex flex-col mx-auto'>
-          
+        <div className='flex flex-col mx-auto bg-zinc-900 rounded-xl border border-zinc-800 p-3'>
+          <h3 className='text-center'> Habit Tracking </h3>
+          <p className='text-center'>Track your daily habits and build consistency</p>
         </div>
 
-        <div className='flex flex-col mx-auto'>
-          a
+        <div className='flex flex-col mx-auto bg-zinc-900 rounded-xl border border-zinc-800 p-3'>
+         <h3 className='text-center'> Progress Analytic</h3>
+         <p className='text-center'>See your evolution over time with clear insights</p>
         </div>
 
       </div>
