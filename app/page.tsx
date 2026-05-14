28e553b91b1fb5a93424ca25ba0030d8acd0af7c
ericferrazp <ericferrazp@gmail.com>
2026-05-14 10:16:34 -0300

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
-      <section className="flex flex-col mx-auto px-4 py-2 items-center mt-60 max-w-3xl">
-        <h1 className ="mb-2 text-red-200 font-bold text-4xl">
+      <section className="flex flex-col mx-auto px-4 py-2 items-center mt-20 max-w-3xl">
+        <h1 className ="mb-2 text-emerald-500 font-bold text-4xl">
           FocusBrew! Your assistant app to Podomoro!
         </h1>
         <p className ="mb-7 text-2xl">
@@ -39,27 +39,30 @@ export default function Home() {
       </div>
       </section>
 
-      <section className='flex flex-col text-center mx-auto max-w-3xl'>
-    <h2> Simple, transparent pricing</h2>
-    <p>And very affordable</p>
-    <div className='grid grid-cols-2 gap-6 ' >
+  <section className='flex flex-col text-center mx-auto max-w-3xl mb-1'>
+      <h2 className='mt-7'> Simple, transparent pricing</h2>
+      <p className='mb-7'>And very affordable</p>
+    <div className='grid grid-cols-2 gap-6 mb-3' >
       <div className='bg-zinc-800 rounded-xl border border-zinc-600 p-6'>
         <h3> Freemium </h3>
         <p> 0,00$</p>
-        <p> Limited performance... </p>
+        <p className='mb-2'> Limited performance... </p>
         <Button className='text-1xl py-6 px-4 mx-auto'> Buy now </Button>
       </div>
 
       <div className='bg-emerald-800 rounded-xl border-2 border-emerald-500 p-6'>
         <h3> Pro Pass </h3>
         <p> 20,00$ </p>
-        <p> MAXIMUM performance! </p>
+        <p className='mb-2'> MAXIMUM performance! </p>
         <Button className='text-1xl py-6 px-4 mx-auto'> Buy now </Button>
       </div>
-
     </div>
+  </section>
 
-      </section>
+  <footer className='text-center mx-auto'>
+    <p className='text-sm text-emerald-500'>© 2026 FocusBrew. All rights reserved.</p>
+    <p className='text-sm'> Link1 Link2 Link3</p>
+  </footer>
 
     </main>
   );
